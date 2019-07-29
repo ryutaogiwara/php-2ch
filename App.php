10994a330b04aeb952f9718a953e16d1c15d2199
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2019-07-29 18:42:49 +0900

    Update App.php

--- App.php
+++ App.php
@@ -1,13 +1,19 @@
 <?php
+// クラスとはデータの設計図のようなもの。
+// そのままでは使うことはできず、メソッドを使うことで取り出すことができる
 class App
 {
+  // runメソッドを定義。
   public function run()
   {
+    // Data.phpの読み込み
     require('Data.php');
+    // Dataクラス（処理）を一つの変数＄dataとして定義（インスタンス化）
     $data = new Data;
-
+    // dataインスタンス内のgetTreadsメソッドを実行し結果を$threadsと定義
     $threads = $data->getThreads();
 
+    // ob_start,ob_get_cleanはセットで使うことで出力のタイミングを任意に調整できる（間に処理を入れることができる）
     ob_start();
     require('Board.php');
     $html = ob_get_clean();
