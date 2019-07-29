086576d6afb70bf4331c65d2dcdddf4bef6ee4d7
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2019-07-29 17:59:32 +0900

    Update App.php
    app.phpの読み込みapp
    app.php内のAppクラスをインスタンス化
    インスタンス変数＄appでrunメソッドを実行し、スレッドを表示

--- App.php
+++ App.php
@@ -1,1 +1,17 @@
 <?php
+class App
+{
+  public function run()
+  {
+    require('Data.php');
+    $data = new Data;
+
+    $threads = $data->getThreads();
+
+    ob_start();
+    require('Board.php');
+    $html = ob_get_clean();
+
+    echo $html;
+  }
+}
