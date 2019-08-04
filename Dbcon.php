062bdb8a61665d762b7847e26f48edaa81dba078
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2019-08-04 13:15:48 +0900

    Add pdo errormode
    PDO接続エラー時にエラーを知らせるメソッド
    これがないとエラー時にエラーであることを返してこないため

--- Dbcon.php
+++ Dbcon.php
@@ -9,8 +9,12 @@ class Dbcon
     $password = 'root';
     $options = array(PDO::MYSQL_ATTR_INIT_COMMAND =>"SET CHARACTER SET 'utf8'");
     $dbh = new PDO($dsn, $user, $password, $options);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $dbh;
+  }
     
-
+  public function postThread()
+  {
     // 条件分岐ー成功時の処理
     try {
       $user_name = 'user';
