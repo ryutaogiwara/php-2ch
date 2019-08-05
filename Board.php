be4844af117b6d9391dd066366b990ed1e61f9af
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2019-08-05 17:07:44 +0900

    Update Board.php
    入力フォーム修正

--- Board.php
+++ Board.php
@@ -8,8 +8,14 @@
   <h1>2ch</h1>
   <div>
     <form action="http://localhost:8002/post" method="post">
-      <label for="post">投稿者</label><textarea id="text" name=user_name cols="20" rows="1" maxlength="20" placeholder="投稿者名"></textarea><br>
-      <textarea id="text" name="body" cols="50" rows="6" maxlength="255" placeholder="投稿内容"></textarea>
+      <div>
+        <label for="user_name">投稿者：</label><br>
+        <textarea id="text" name="user_name" cols="20" rows="1" maxlength="20" placeholder="投降者名"></textarea>
+      </div>
+      <div>
+        <label for="body">投稿内容：</label><br>
+        <textarea id="text" name="body" cols="50" rows="6" maxlength="255" placeholder="投稿内容"></textarea>
+      </div>
       <p><input type="submit" value="送信"></p>
     </form>
   </div>
