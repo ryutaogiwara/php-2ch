495a95a125b1fe517936faae45839384c6bb6897
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2019-08-08 13:44:18 +0900

    Update Board.php
    若干の表記修正

--- Board.php
+++ Board.php
@@ -21,17 +21,10 @@
   </div>
   <?php
   for ($i = 0; $i < count($threads); $i += 1) {
-    if ($i % 3 === 0) {
-      echo '<div>' . $threads[$i]['id'] . '</div>';
-      echo '<div style="color:blue;">' . $threads[$i]['name'] . '</div>';
-      echo '<div>' . $threads[$i]['body'] . '</div>';
-      echo '</br>';
-    } else {
-      echo '<div>' . $threads[$i]['id'] . '</div>';
-      echo '<div style="color:blue;">' . $threads[$i]['name'] . '</div>';
-      echo '<div>' . $threads[$i]['body'] . '</div>';
-      echo '</br>';
-    }
+    echo '<div>' . $threads[$i]['id'] . '</div>';
+    echo '<div style="color:blue;">' . $threads[$i]['user_name'] . '</div>';
+    echo '<div>' . $threads[$i]['body'] . '</div>';
+    echo '</br>';
   }
   ?>
 </body>
